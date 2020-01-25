a343536d13cbd91cdc8c5ba7c3f6ed09b6601740
Julian Benda <julian.benda@st.ovgu.de>
2020-01-25 13:29:56 +0100

    Added Model for DB data

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevTaskJulianBenda.Models
+{
+    public class Product
+    {
+        public long id { get; set; }
+        public long product_id { get; set; }
+        public string product_name { get; set; }
+        public long stock_available { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
