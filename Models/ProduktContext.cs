0d04d71ffecb8b3958ae638c0655b2525df856c5
Julian Benda <julian.benda@st.ovgu.de>
2020-01-25 14:12:38 +0100

    Minimal working DB access

---
+++ Models/ProduktContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevTaskJulianBenda.Models
+{
+    public class ProduktContext : DbContext
+    {
+        public ProduktContext(DbContextOptions<ProduktContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using DevTaskJulianBenda.Models;
 
 namespace DevTaskJulianBenda
 {
@@ -25,6 +27,8 @@ namespace DevTaskJulianBenda
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProduktContext>(opt =>
+             opt.UseSqlServer("Server=(localdb)\\MyTestDB;Database=StoreDB;"));
             services.AddControllers();
         }
 
