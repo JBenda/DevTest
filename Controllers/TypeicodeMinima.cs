ae99922a4902525af255334bc27b879fdbdd8ece
Julian Benda <julian.benda@st.ovgu.de>
2020-01-25 12:22:53 +0100

    Added typeicodeminima API
    Method          Route              	Respond
    GET       /api/typeicodeminima  list of objects with minima in body
    
    source for typeicodes: https://jsonplaceholder.typicode.com/posts

---
+++ Controllers/TypeicodeMinima.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DevTaskJulianBenda.Models;
+
+namespace DevTaskJulianBenda.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeicodeMinima : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+        [HttpGet]
+        public async Task<IEnumerable<Typeicode>> GetTypeicodesWithMinima() {
+            string respondString = await client.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+            return JsonSerializer.Deserialize<IEnumerable<Typeicode>>(respondString)
+                .Where(code => code.body.Contains("minima"));
+        }
+    }
+}
