296b9647f291bbddab0f7fb2493734d0ad2fef7a
Julian Benda <julian.benda@st.ovgu.de>
2020-01-25 14:38:28 +0100

    Added product listening
    Method          Route                   Respond
     GET       /api/Products           list of all Products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -22,88 +22,13 @@ namespace DevTaskJulianBenda.Controllers
 
         // GET: api/Products
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public IEnumerable<ProductList> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            return _context.Products.Select<Product, ProductList>(product => new ProductList{
+                id = product.id, prodcut_name = product.product_name, product_id = product.product_id
+            }) ;
         }
+      
 
-        // GET: api/Products/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(long id)
-        {
-            var product = await _context.Products.FindAsync(id);
-
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return product;
-        }
-
-        // PUT: api/Products/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(long id, Product product)
-        {
-            if (id != product.id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(product).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Products
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
-        {
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetProduct", new { id = product.id }, product);
-        }
-
-        // DELETE: api/Products/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Product>> DeleteProduct(long id)
-        {
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
-
-            return product;
-        }
-
-        private bool ProductExists(long id)
-        {
-            return _context.Products.Any(e => e.id == id);
-        }
     }
 }
--- Models/ProductList.cs
+++ Models/ProductList.cs
@@ -5,13 +5,10 @@ using System.Threading.Tasks;
 
 namespace DevTaskJulianBenda.Models
 {
-    public class Product
+    public class ProductList
     {
         public int id { get; set; }
         public int product_id { get; set; }
-        public string product_name { get; set; }
-        public int stock_available { get; set; }
-        public TimeSpan created_at { get; set; }
-        public TimeSpan updated_at { get; set; }
+        public string prodcut_name { get; set; }
     }
 }
